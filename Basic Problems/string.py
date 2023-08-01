8950416deccdf1c446d42cb1b273c0a2e93f5d9d
Ashwini Koli <ashwini.koli1501@gmail.com>
2023-08-01 18:56:18 +0530

    some initial changes

--- Basic Problems/string.py
+++ Basic Problems/string.py
@@ -1,4 +1,13 @@
-str = "ashwini"
+str = "ashwini koli"
 print(str)
 print(str[1])
 print(str[2:5])
+print(len(str))
+
+if "hello" in str:
+    print("yes this name is present in the string...")
+else:
+    print("sorry we can't find this name....")
+
+if str in str:
+    print(str)
